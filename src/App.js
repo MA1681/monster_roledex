d8dbfef189473b6dfff2cd264b1cbe981bfb0865
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2024-02-05 17:50:13 +0000

    improving code performence

--- src/App.js
+++ src/App.js
@@ -9,29 +9,54 @@ class App extends Component {
     super();
 
     this.state = { 
-    monsters:
-    
-    [ 
-    {name:'Linda',id:1},
-    {name:'Bella',id:2},
-    {name:'Jack',id:3},
-    {name:'Samson',id:4},
-    {name:'Regan',id:5}
-    ]
-   
-    };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    monsters:[],
+    searchField:''
+    };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users') //getting data from some site
+    .then((response) => response.json())   //transforming this data into JSON (readable data)
+    .then((users) => this.setState(() => {  //setting our current State to the new State as feched data
+      return {monsters: users}
+    }, 
+    () => {console.log(this.state)}  // optional if we want to see in console our feched data
+    ));
+  }
+
+  onSearchChange = (event) => {console.log(event.target.value); //listenning changes what we type in input field
+    const searchField=event.target.value.toLocaleLowerCase();
+    this.setState(()=>{
+    return {searchField};
+    });
+
   }
   
   render() {
-     
-  return <div className="App"> 
-    {
-      this.state.monsters.map((monster)=>{
+  
+  const {monsters,searchField} = this.state;
+  const {onSearchChange} = this;
+
+  const filteredMonsters=monsters.filter((monster) => {  //filtering monsters name that are included what we type in input
+   return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
+  return (
+  
+  <div className="App"> 
+  <input 
+  className='serach-box' 
+  type='search' 
+  placeholder='search monsters' 
+  onChange={onSearchChange} 
+  />
+    
+    { filteredMonsters.map((monster) => {   //getting our monster mapping throug the array one by one
       return <div key={monster.id}><h1 > {monster.name} </h1></div>
       })
     }
-
     </div>
+  );
   }
 }
 
